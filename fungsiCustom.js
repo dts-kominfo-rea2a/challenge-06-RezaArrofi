119dd89d14c798d9c5fe8acafebd6a3ef1535c91
RezaArrofi <arrofireza23@gmail.com>
2022-06-12 13:36:28 +0700

    build in function

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,47 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCb) => {
+  let arrHasil = [];
+
+  // baca data pertama
+  fs.readFile(
+    file1, {encoding:"utf8"}, (err, data) => {
+      if(err){
+        return fnCb(err, null);
+      }
+
+      const hasilData = JSON.parse(data);
+      arrHasil.push(hasilData.message.split(" ")[1]);
+    }
+  );
+
+  // baca file kedua
+  fs.readFile(
+    file2, {encoding: "utf-8"}, (err, data) => {
+      if(err){
+        return fnCb(err, null);
+      } 
+      
+      const hasilData = JSON.parse(data);
+      arrHasil.push(hasilData[0].message.split(" ")[1]);  
+    }
+  )
+
+  // baca file ketiga
+  fs.readFile(
+    file3, { encoding: "utf8" }, (err, data) => {
+      if (err) {
+        return fnCb(err, null);
+      }
+
+      const hasilData = JSON.parse(data);
+      arrHasil.push(hasilData[0].data.message.split(" ")[1]);
+      fnCb(null, arrHasil);
+    }
+  )
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
